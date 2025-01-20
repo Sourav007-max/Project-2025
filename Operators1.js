569710f38a0c969eaec2b9a1fedbfdfbedf390c0
Sourav007-max <dhayapules@gmail.com>
2025-01-20 11:46:08 +0530

    Operators file

---
+++ Operators1.js
@@ -0,0 +1,25 @@
+console.log(10+2); // Addition Operator
+
+console.log(10-7); // Subtraction Operator
+
+console.log(10*7); // Multiplication Operator
+
+console.log(10/7); // Division Operator
+
+console.log(10%3); // Modules Operator
+
+console.log(10**7); // Exponentaition Operator
+
+var a = 10;
+console.log(a++); // Post Increment operator
+console.log(a);
+
+console.log(++a); // Pre Increment operator
+
+console.log(a--); // Post Decrement operator
+
+console.log(--a); // Pre Decrement operator
+
+// Checks Value and Data Type Both
+
+console.log(10 == 10); // True Value
