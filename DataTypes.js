065827e3eb88616f74d969e0ab0c6db3ae9b05dd
Sourav007-max <dhayapules@gmail.com>
2025-01-21 13:04:11 +0530

    Data Types

---
+++ DataTypes.js
@@ -0,0 +1,28 @@
+// Number Data Type: All number like (whole, decimal, positive, negative)
+var a = 12;
+console.log(a, typeof a);
+
+// Strings: single characters or series of characters all are String
+
+var b = 'A';
+console.log(b, typeof b);
+
+// Boolean: True / False
+
+var c = true;
+console.log(c, typeof c);
+
+// undefined:
+
+var d;
+console.log(d, typeof d); // Undefined
+
+// null
+
+var e = null;
+console.log(e, typeof e); // Null
+
+// Infinity : any numbers divided by 0
+
+var f = 123/0;
+console.log(f, typeof f);
